003bdc4548ff2e43a13597fc1f6f6f1b6ae5f078
ismet kizgin <info@ismetkizgin.com.tr>
2024-04-04 21:03:11 +0300

    test: update plugin traverser test

--- src/plugin-traverser.spec.ts
+++ src/plugin-traverser.spec.ts
@@ -14,6 +14,16 @@ describe('PluginTraverser', () => {
     expect(result).toHaveLength(2);
   });
 
+  it('Should traverse multiple directories and import modules correctly.', async () => {
+    const mockPluginModuleOptions: PluginModuleOptions = {
+      directories: [path.join(__dirname, '../test/fixtures')],
+    };
+    const pluginTraverser = new PluginTraverser(mockPluginModuleOptions);
+    const result = await pluginTraverser.traverseDirectories();
+
+    expect(result).toHaveLength(2);
+  });
+
   it('Should traverse directories and import modules correctly. (node_modules)', async () => {
     const mockPluginModuleOptions: PluginModuleOptions = {};
     const pluginTraverser = new PluginTraverser(mockPluginModuleOptions);
